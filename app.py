82e884738a9aa662a1e7b7a620b317deaed750b2
johnnie71 <johnnie.agonz71@gmail.com>
2021-12-14 18:16:38 -0500

    creating a connection to the sql DB

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
+
+def db_connection(): ## creating the connection to the sql database
+    connection = None
+    try:
+        connection = sqlite3.connect('books.sqlite')
+    except sqlite3.error as e:
+        print(e)
+    return connection
+
 
 @app.route('/')
 def index():
