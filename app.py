72b84d45adf4492d0682ce03a13a86192debb30a
johnnie71 <johnnie.agonz71@gmail.com>
2021-12-14 18:31:21 -0500

    get request method for getting db data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import sqlite3
 
 app = Flask(__name__)
@@ -10,6 +10,35 @@ def db_connection(): ## creating the connection to the sql database
     except sqlite3.error as e:
         print(e)
     return connection
+
+@app.route('/books', methods=["GET", "POST"])
+def books():
+    connection = db_connection()
+    cursor = connection.cursor()
+
+    if request.method == "GET":
+        cursor = connection.execute('SELECT * FROM books')
+        books = [
+            dict(id=row[0], author=row[1], language=row[2], title=row[3])
+            for row in cursor.fetchall()
+        ]
+        if books is not None:
+            return jsonify(books)
+
+    if request.method == "POST":
+        print("get post req")
+        new_author = request.form["author"]
+        new_lang = request.form["language"]
+        new_title = request.form["title"]
+        sql = """INSERT INTO books (author, lanuage, title) VALUES (?, ?, ?)"""
+        new_obj = {
+            "id": id,
+            "author": new_author,
+            "language": new_lang,
+            "title": new_title
+        }
+    
+
 
 
 @app.route('/')
