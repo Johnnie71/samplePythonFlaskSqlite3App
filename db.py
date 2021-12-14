ee8f0cc8332645054baf68e03a73be693889739d
johnnie71 <johnnie.agonz71@gmail.com>
2021-12-14 17:51:26 -0500

    created a cursor to be able to execute sql statements to the database

--- db.py
+++ db.py
@@ -2,3 +2,4 @@ import sqlite3
 
 connect = sqlite3.connect('books.sqlite') #creating the database
 
+cursor = connect.cursor() # creating a cursor to be able to execute sql statements on the database
