a6aae86be4e39f8e558977ca823a5210e136ca2f
johnnie71 <johnnie.agonz71@gmail.com>
2021-12-14 17:54:54 -0500

    creating table

--- db.py
+++ db.py
@@ -2,4 +2,12 @@ import sqlite3
 
 connect = sqlite3.connect('books.sqlite') #creating the database
 
-cursor = connect.cursor() # creating a cursor to be able to execute sql statements on the database
+cursor = connect.cursor() # creating a cursor Object to be able to execute sql statements on the database
+
+# creating the table variable
+sql_query = """ CREATE TABLE books (
+    id integer PRIMARY KEY,
+    author text NOT NULL,
+    language text NOT NULL,
+    title text NOT NULL
+)"""
