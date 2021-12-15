3923099cf9c380a1ef330fcf5cb1c71df641d927
johnnie71 <johnnie.agonz71@gmail.com>
2021-12-15 16:02:25 -0500

    show oage for book

--- app.py
+++ app.py
@@ -47,48 +47,22 @@ def books():
         connection.commit()
         return redirect(url_for('index'))
     
-@app.route("/book/<int:id>", methods=["GET", "PUT", "DELETE"])
+@app.route("/book/<int:id>")
 def single_book(id):
     connection = db_connection()
     cursor = connection.cursor()
     book = None
-    if request.method == "GET":
-        cursor.execute("SELECT * FROM books WHERE id=?", (id))
-        rows = cursor.fetchall()
-        for r in rows:
-            book = r
-        if book is not None:
-            print(book)
-            return render_template("book.html", book=book)
-        else:
-            return "Something went wrong!", 404
-
-    if request.method == "PUT":
-        sql = """UPDATE book SET 
-            title=?,
-            author=?,
-            language=? 
-            WHERE id=?
-        """
-        author = request.form["author"]
-        language = request.form["language"]
-        title = request.form["title"]
-        updated_book = {
-            "id": id,
-            "author": author,
-            "language": language,
-            "title": title
-        }
-        connection.execute(sql, (title, author, language, id))
-        connection.commit()
-        return jsonify(updated_book)
     
-    if request.method("DELETE"):
-        sql = """ DELETE FROM book WHERE id=? """
-        connection.execute(sql, (id))
-        connection.commit()
+    cursor.execute("SELECT * FROM books WHERE id=?", (id))
+    rows = cursor.fetchall()
+    for r in rows:
+        book = r
+    if book is not None:
+        print(book)
+        return render_template("book.html", book=book)
+    else:
+        return "Something went wrong!", 404
 
-
-
+    
 if __name__ == "__main__":
     app.run(debug=True)
